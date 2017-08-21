3bdaae3511ce94c86d94873e0b75a0ae502cc684
zhengweifu_win <zhengweifu@bitmap3d.com.cn>
2017-08-21 10:08:14 +0800

    Mod: src/index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-export { default as Col } from './components/Col';
+export { default as Col } from './components/Grid/Col';
+export { default as GridList } from './components/Grid/GridList';
+export { default as Grid } from './components/Grid/Grid';
+
+export { default as IconButton } from './components/Button/IconButton';
+export { default as FlatButton } from './components/Button/FlatButton';
+export { default as RaisedButton } from './components/Button/RaisedButton';
+
 export { default as ColorGroup } from './components/ColorGroup';
 export { default as ColorItem } from './components/ColorItem';
 export { default as Float } from './components/Float';
-export { default as GridList } from './components/GridList';
-export { default as Grid } from './components/Grid';
-export { default as IconButton } from './components/IconButton';
 export { default as ImageItem } from './components/ImageItem';
 export { default as Input } from './components/Input';
 export { default as InputNumber } from './components/InputNumber';
@@ -14,15 +18,18 @@ export { default as Label } from './components/Label';
 export { default as Left } from './components/Left';
 export { default as List } from './components/List';
 export { default as ListCloseSwitch } from './components/ListCloseSwitch';
-export { default as Menu } from './components/Menu';
-export { default as MenuItem } from './components/MenuItem';
+
+export { default as Menu } from './components/Menu/Menu';
+export { default as SubMenu } from './components/Menu/SubMenu';
+export { default as MenuItem } from './components/Menu/MenuItem';
+export { default as MenuItemGroup } from './components/Menu/MenuItemGroup';
+
 export { default as Modal } from './components/Modal';
 export { default as NavBar } from './components/NavBar';
 export { default as Overlay } from './components/Overlay';
 export { default as Paper } from './components/Paper';
 export { default as Popover } from './components/Popover';
 export { default as PopuPanel } from './components/PopuPanel';
-export { default as RaisedButton } from './components/RaisedButton';
 export { default as Rular } from './components/Rular';
 export { default as Select } from './components/Select';
 export { default as SimpleItem } from './components/SimpleItem';
